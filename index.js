f539280a358553e8a1a2317cb86fa78e8ab8f203
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-11-26 00:09:27 +0600

    Apartments data has been posted manually in database and appartments part has been completed using get method.

--- index.js
+++ index.js
@@ -25,7 +25,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const apartmentCollection = client.db("buildingManagement").collection("apartments");
+
+    app.get('/apartments', async (req, res) => {
+      const result = await apartmentCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
